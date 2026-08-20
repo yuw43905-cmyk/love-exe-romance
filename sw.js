bc13f104aad137b35603630492742636828a4c2c
yuw43905-cmyk <yuw43905@gmail.com>
2026-08-20 14:48:47 +0800

    Force-refresh the new heart style in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "love-exe-v5";
-const ASSETS = ["./", "./app.css", "./matrix.css", "./app.js", "./manifest.webmanifest"];
+const CACHE = "love-exe-v6";
+const ASSETS = ["./", "./app.css", "./matrix.css?v=heart3", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((response) => response || caches.match("./")))));
